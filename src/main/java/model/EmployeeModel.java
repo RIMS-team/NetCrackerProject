4d3252e9fde6f7e9eccd869b5c59f6f2996ed80f
barmin <barminstas@gmail.com>
2016-11-23 17:45:04 +0200

    EmployeeModel

---
+++ src/main/java/model/EmployeeModel.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * Created by barmin on 23.11.2016.
+ */
+public interface EmployeeModel {
+     int getEmployeeID();
+
+     void setEmployeeID(int employeeID);
+
+     int getPhoneNumber();
+
+     void setPhoneNumber(int phoneNumber);
+
+     String getFullName();
+
+     void setFullName(String fullName);
+
+     String geteMail();
+
+     void seteMail(String eMail);
+}
---
+++ src/main/java/model/impl/EmployeeImpl.java
@@ -0,0 +1,45 @@
+package model.impl;
+
+import model.EmployeeModel;
+
+/**
+ * Created by barmin on 23.11.2016.
+ */
+public class EmployeeImpl implements EmployeeModel {
+    int employeeID;
+    int phoneNumber;
+    String fullName;
+    String eMail;
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+}
