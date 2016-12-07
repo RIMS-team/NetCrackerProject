73d2e0fdbccd85b4f21adc6ab106dc1554c19b7a
Dima Salsky <sdiker@ukr.net>
2016-12-07 18:22:26 +0200

    UserMapper returned

---
+++ src/main/java/dao/Mappers/UserMapper.java
@@ -0,0 +1,26 @@
+package dao.Mappers;
+
+import model.Employee;
+import model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by trvler135 on 06.12.2016.
+ */
+public class UserMapper implements RowMapper<User> {
+
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User(
+                rs.getInt("EMPLOYEE_ID"),
+                rs.getInt("PHONE_NUMBER"),
+                rs.getString("FULL_NAME"),
+                rs.getString("EMAIL"),
+                rs.getString("PASSWORD")
+        );
+        return user;
+    }
+}
+
